141e3a4858405d0e53ec24b9f9330f25cebebe32
Ubuntu <ubuntu@ip-172-31-52-158.us-west-2.compute.internal>
2022-09-24 03:58:32 +0000

    new

---
+++ Model/run.py
@@ -0,0 +1,25 @@
+import os
+import threading
+
+
+def run_one_config(workload, datatype, operation):
+    os.system('python main.py --workload=' + workload + ' --datatype=' + datatype + ' --operation=' + operation + ' > ' + workload + '_' + datatype + '_' + operation + '.txt')
+        
+
+threads = []
+workloads = ['pixelfly_block2', 'pixelfly_block4', 'pixelfly_block8', 'pixelfly_block16', 'pixelfly_block32']
+datatypes = ['BF16']
+operations = ['centralized', 'data', 'model', 'hybrid']
+
+for workload in workloads:
+    for datatype in datatypes:
+        for operation in operations:
+            thread = threading.Thread(target=run_one_config, args=(workload, datatype, operation))
+            threads.append(thread)
+            
+    
+for i in range(len(threads)):
+    threads[i].start()
+   
+for i in range(len(threads)):
+    threads[i].join()
---
+++ submit.py
@@ -0,0 +1,7 @@
+import os
+
+os.system('git add .')
+os.system('git commit -m "new"')
+os.system('git push origin master')
+
+
